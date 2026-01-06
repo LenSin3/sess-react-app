018bcb3122cde755ebd687c5508bfa689953d7fe
Leonard Paul-Kamara <lenpk3@yahoo.co.uk>
2026-01-06 04:09:31 -0600

    Initialize App component with routing and layout

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Mission from './components/Mission'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import './App.css'
+
+function HomePage() {
+    return (
+          <>
+                <Hero />
+                <Mission />
+          </>>
+        )
+}
+
+function App() {
+    return (
+          <div className="app">
+                <Navbar />
+                <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/services" element={<Services />} />
+                        <Route path="/contact" element={<Contact />} />
+                </Routes>Routes>
+                <Footer />
+          </div>div>
+        )
+}
+
+export default App</>
